f1d87cefae874962fc1f40ec23b449f7594a5d2e
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2021-06-06 20:51:07 +0600

    null vs undefined cleared

---
+++ Desktop/advanced js/null-vs-undefined.js
@@ -0,0 +1,18 @@
+//  function add(num1, num2 ){ 
+//      console.log(num1 + num2)
+//      return 
+
+//  }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// } 
+
+// const premik = {name: "throw", number: 0000000000};
+// console.log(premik.gf)
+
+// let fun = undefined; 
+// console.log(fun) 
+
+let ages =[2,5,5]
+console.log(ages[22])
