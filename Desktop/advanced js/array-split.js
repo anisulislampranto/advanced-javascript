e1d03e8e7d60a14daed332ccb77ba0ec591cc576
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2021-06-08 11:55:10 +0600

    slice-splice-join cconcept clear

---
+++ Desktop/advanced js/array-split.js
@@ -0,0 +1,13 @@
+const nums = [81,92,73,04,65,36,27,48];
+// const part = nums.slice(2, 6);
+
+const removed = nums.splice(2,4, 23, 34, 56,78,);
+
+// console.log(removed);
+// console.log(nums);
+
+
+const together = nums.join(" gap ");
+console.log(together);
+
+
